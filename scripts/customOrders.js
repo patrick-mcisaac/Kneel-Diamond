521ee228c9a999f019617df684301c56e968987e
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-07-30 19:34:05 -0500

    fix

--- scripts/customOrders.js
+++ scripts/customOrders.js
@@ -4,11 +4,15 @@ export const Orders = async () => {
     const response = await fetch('http://localhost:8088/orders?_expand=style&_expand=size&_expand=metal')
     const orders = await response.json()
 
-    return orders.map((order) => {
-        return `
-        <article class='orders'>
-            <p>Order #${order.id} cost <span class='priceSpan'>$${(order.metal.price + order.style.price + order.size.price).toFixed(2)}</span> </p>
-        </article>
-        `
-    }).join('')
+    if(orders.length){
+        return orders.map((order) => {
+            return `
+            <article class='orders'>
+                <p>Order #${order.id} cost <span class='priceSpan'>$${(order.metal.price + order.style.price + order.size.price).toFixed(2)}</span> </p>
+            </article>
+            `
+        }).join('')
+    }else {
+        return ''
+    }
 }
